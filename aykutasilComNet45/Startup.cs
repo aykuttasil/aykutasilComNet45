9eced6e9df25c1c8891d7073fd2cadcdd518593f
Aykut <aykut.asil@aktif.com>
2015-10-15 18:02:21 +0300

    first commit

---
+++ aykutasilComNet45/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(aykutasilComNet45.Startup))]
+namespace aykutasilComNet45
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
